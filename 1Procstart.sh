e7ada33d87a33bde8eb44859f5c2bdffe2e3dccb
phanirithvij <phanirithvij20003@gmail.com>
2018-02-03 18:15:51 +0530

    Run fun.sh instead of testforall.sh

--- 1Procstart.sh
+++ 1Procstart.sh
@@ -4,8 +4,12 @@ cp testforall.sh www.funmanga.com/uploads/chapters/
 cp indvtest.sh www.funmanga.com/uploads/chapters/
 cp wget.sh www.funmanga.com/uploads/chapters/
 cp wget.sh www.funmanga.com/uploads/chapters/1051/
+cp fun.sh  www.funmanga.com/uploads/chapters/1051/
 cp README.md www.funmanga.com/uploads/chapters/
 cp chapters.txt www.funmanga.com/uploads/chapters/
+cp fun.sh www.funmanga.com/uploads/chapters/
+cp jquery-1.7.1.min.js www.funmanga.com/uploads/chapters/
+cp script.js www.funmanga.com/uploads/chapters/
 cp style.txt www.funmanga.com/uploads/chapters/
 cp script.txt www.funmanga.com/uploads/chapters/
 cp content.txt www.funmanga.com/uploads/chapters/
@@ -13,6 +17,6 @@ cp examples.txt www.funmanga.com/uploads/chapters/
 cp linkget.sh www.funmanga.com/uploads/chapters/
 cp jquery-1.12.4.min.js www.funmanga.com/uploads/chapters/
 echo Do this
-echo cd www.funmanga.com/uploads/chapters/
-
-#chapters.txt  examples.txt  important.txt  indvtest.sh  linkget.sh  README.md  testforall.sh  wget.sh style.txt script.txt content.txt 
+echo 1.  cd www.funmanga.com/uploads/chapters/
+echo 2.  run fun.sh or testforall.sh
+#chapters.txt  examples.txt  important.txt  indvtest.sh  linkget.sh  README.md  testforall.sh  wget.sh style.txt script.txt content.txt fun.sh oldjquery script.js
